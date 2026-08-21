dde29ce7c4bcf78bb6fa70e78ff8e44526349500
Fernando <fernandojczaban@gmial.com>
2026-08-21 11:48:55 -0300

    gdf

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState, useEffect, useRef } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
